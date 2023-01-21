e2e0cd2331e022f0ae39eb38018868ef5bee4024
panmaster007 <fullstack@naladka.su>
2023-01-21 12:27:07 +0300

    Add Example002_HelloUser/Example002_HelloUser

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите Ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
