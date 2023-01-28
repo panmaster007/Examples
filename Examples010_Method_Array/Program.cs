a0efbd280c819d513431b91b6e492d4df0a707b8
Igor <fullstack@naladka.su>
2023-01-28 20:51:01 +0300

    Edit Example010

--- Examples010_Method_Array/Program.cs
+++ Examples010_Method_Array/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = { 1, 12, 31, 4, 18, 16, 17, 18 };
+
+int n = array.Length;
+int find = 31;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
