be5c8052f888992be829fe310b6d83486847d03d
Igor <fullstack@naladka.su>
2023-01-28 23:34:08 +0300

    Edit Example011

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] ints= new int[10];
+
+FillArray(ints);
+PrintArray(ints);
