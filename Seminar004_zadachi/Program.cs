f543f8ef89ce231eef1a694b5cc320b7ad8e26df
Igor <fullstack@naladka.su>
2023-01-28 19:05:02 +0300

    Add Example008

--- Seminar004_zadachi/Program.cs
+++ Seminar004_zadachi/Program.cs
@@ -1,5 +1,7 @@
-﻿// Console.WriteLine("Введите число N");
-// while
-// {
-
-// }
+﻿Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = n * -1;
+while (count <= n)
+{
+    Console.WriteLine(count ++);
+}
